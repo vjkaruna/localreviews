205047e28ea92099c4b3d6db3e2c4cd9d1de0ed5
vjkaruna <vjkaruna@gmail.com>
2014-09-12 14:33:21 -0700

    first commit

---
+++ CineReviews/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  CineReviews
+//
+//  Created by Vijay Karunamurthy on 9/10/14.
+//  Copyright (c) 2014 vjkaruna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        requestAPIData()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "")
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    func requestAPIData() {
+        
+        var url: String = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=mkwnbrc7g34k64vpq96nshc9"
+        var url2: String = "http://www.cnn.com"
+        
+        var request = NSMutableURLRequest(URL: NSURL(string: url))
+        request.setValue("*/*",forHTTPHeaderField:"Accept")
+        request.HTTPMethod = "GET"
+        request.setValue("curl/7.37.1", forHTTPHeaderField:"User-Agent")
+        
+        println("\(request)")
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            println("\(error)")
+            var object: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0) , error: nil)
+            println("\(object)")
+            })
+    }
+    
+}
+
