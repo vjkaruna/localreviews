77feadb706230628902418e342dfce348eb22dc8
vjkaruna <vjkaruna@gmail.com>
2014-09-14 14:04:45 -0700

    SDWebImage cache, SwiftyJSON API, and thumbnail URLs in prototype Table View Cells.

---
+++ CineReviews/Movies.swift
@@ -0,0 +1,19 @@
+//
+//  Movies.swift
+//  CineReviews
+//
+//  Created by Vijay Karunamurthy on 9/14/14.
+//  Copyright (c) 2014 vjkaruna. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    
+    var title: String = ""
+    var synopsis: String = ""
+    
+    var thumbURL: String = ""
+    var posterURL: String = ""
+    
+}
--- CineReviews/ViewController.swift
+++ CineReviews/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 import Alamofire
 
 
+
 class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var movieTableView: UITableView!
+    var movies = [Movie]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +34,17 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .Default, reuseIdentifier: "")
-        cell.textLabel?.text = "Row \(indexPath.row)"
+        //let cell = UITableViewCell(style: .Default, reuseIdentifier: "")
+        let cell = tableView.dequeueReusableCellWithIdentifier("mvItemCell") as UITableViewCell
+        let nameLabel: UILabel = cell.viewWithTag(102) as UILabel
+        let thumbnail: UIImageView = cell.viewWithTag(101) as UIImageView
+        if (indexPath.row < movies.count) {
+            nameLabel.text = "\(movies[indexPath.row].title)"
+            thumbnail.sd_setImageWithURL(NSURL(string: movies[indexPath.row].thumbURL))            
+        } else {
+            nameLabel.text = "Row \(indexPath.row)"
+        }
+        // cell.imageView = "http://content8.flixster.com/movie/11/17/72/11177246_tmb.jpg"
         return cell
     }
     
@@ -60,12 +73,39 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         Alamofire.request(.GET, url).responseJSON { (request, response, data, error) in
             if (error != nil) {
-                println("TODO: handle error")
+                println("TODO: handle error 1")
             }
             
-            let JSON = JSONValue(data as AnyObject!)
-            let movie_title = JSON["movies"][0]["title"]
-            println("\(movie_title)")
+            let json = JSONValue(data as AnyObject!)
+            switch json["movies"] {
+                case .JArray(let moviesArray):
+                    for movie in moviesArray {
+                        var movieObj = Movie()
+                        //let movie = movie_item.1
+                        let title = movie["title"].string
+                        let synopsis = movie["synopsis"].string
+                        let thumbURL = movie["posters"]["thumbnail"].string
+                        let posterURL = movie["posters"]["profile"].string
+                        
+                        if (title == nil || synopsis == nil || thumbURL == nil || posterURL == nil) {
+                            println("TODO: handle error 2")
+                        } else {
+                            movieObj.title = title!
+                            movieObj.synopsis = synopsis!
+                            movieObj.thumbURL = thumbURL!
+                            movieObj.posterURL = posterURL!
+                            self.movies.append(movieObj)
+                        }
+                    }
+                default:
+                    let jm = json["movies"]
+                    println("TODO: handle error 3 \(jm)")
+            }
+            for movie in self.movies {
+                println("\(movie.title)")
+            }
+            
+            self.movieTableView.reloadData()
             
         }
         
